cc7d92701e0e45ca52f801221b9da6fd19909328
Joao Santana <joao@hofy.co>
2022-10-27 20:31:18 -0300

    fix seed

--- cmd/posterr/main.go
+++ cmd/posterr/main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"context"
-	"database/sql"
 	"fmt"
 	"log"
 
@@ -16,33 +14,18 @@ func main() {
 		log.Fatalf("error loading config: error=(%v)", err)
 	}
 
-	ctx := context.Background()
 	dbConn, err := db.NewConnection(cfg.DBURL)
 	if err != nil {
 		log.Fatalf("error connecting to database: error=(%v)", err)
 	}
-	querier := db.New(dbConn)
-	handleDatabase(ctx, cfg, dbConn, querier)
-	
-	// TODO: set server and set querier as param
-	fmt.Println("Hello, Posterr!")
-}
+	db.New(dbConn)
 
-func handleDatabase(
-	ctx context.Context,
-	cfg config.Config,
-	dbConn *sql.DB,
-	querier *db.Queries,
-) {
 	if cfg.ShouldMigrate {
 		if err := db.Migrate(dbConn, cfg.DBName, cfg.MigrationPath); err != nil {
 			log.Fatalf("key 'SHOULD_MIGRATE' is (%t) but migration failed: error=(%v)", cfg.ShouldMigrate, err)
 		}
 	}
-
-	if cfg.IsLocal() {
-		if err := querier.SeedUsers(ctx); err != nil {
-			log.Fatalf("seed users failed: error=(%v)", err)
-		}
-	}
+	
+	// TODO: set server and set querier as param
+	fmt.Println("Hello, Posterr!")
 }
